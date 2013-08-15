681f6f4ec4bd876df271e8523e79401771e3d34e
Onur Gungor <onurgu@gmail.com>
2013-08-15 22:47:43 +0300

    Now showing the port that the app is running in console Removed subhandler

--- src/appstartup.py
+++ src/appstartup.py
@@ -5,7 +5,6 @@ import tornado.web
 
 from mainhandler import MainHandler
 from followerhandler import FollowerHandler
-from subhandler import SubHandler
 from schedulerMainHandler import SchedulerMainHandler
 
 def start():
@@ -13,6 +12,6 @@ def start():
         (r"/scheduler/suggestUseridToGet_(friends|followers)", SchedulerMainHandler),
         (r"/(friends|followers)/(ids|list)/?(store|view)?", FollowerHandler),
         ])
-    print "Direnaj Service Layer Started"
+    print "Direnaj Service Layer Starting on port %s" % DIRENAJ_APP_PORT
     application.listen(DIRENAJ_APP_PORT)
     tornado.ioloop.IOLoop.instance().start()
