91a0e0844f207017869f0c12017498d2c3ab2274
Onur Gungor <onurgu@gmail.com>
2013-08-18 15:52:57 +0300

    cosmetic changes in test_followerhandler.py

--- direnaj/tests/test_followerhandler.py
+++ direnaj/tests/test_followerhandler.py
@@ -1,6 +1,3 @@
-
-
-
 import pytest
 
 import tornado.testing
@@ -33,7 +30,6 @@ class TestHandlerBase(AsyncHTTPTestCase):
     #def get_http_port(self):
     #    return DIRENAJ_APP_PORT['test']
 
-# They are runnable via nosetests as well.
 class TestBucketHandler(TestHandlerBase):
     def test_deneme(self):
 
@@ -41,8 +37,6 @@ class TestBucketHandler(TestHandlerBase):
 
         print self.get_http_port()
         # Example on how to hit a particular handler as POST request.
-        # In this example, we want to test the redirect,
-        # thus follow_redirects is set to False
         post_args = {'user_id': '461494372'}
         response = self.fetch(
                             '/followers/ids',
@@ -50,48 +44,28 @@ class TestBucketHandler(TestHandlerBase):
                             body=urllib.urlencode(post_args),
                             follow_redirects=False)
 
+        # http://localhost:9999/followers/ids/view?user_id=461494372
 
-# http://localhost:9999/followers/ids/view?user_id=461494372
-
-# response:
-# TODO: find a way to represent string in multilines! -onurgu
-        expected_response_obj = bson.json_util.loads(u'{"results": [  {                        "friend_id_str": "461494372",                        "retrieved_by": "dummy_data",                        "record_retrieved_at": "Thu May 30 14:20:16 +0000 2013",                        "id_str": "461494360",                        "following": 1,                        "_id": {                            "$oid": "520d2f206ea62501d9c64ac9"                            }                        }                    ]                }')
+        # response:
+        expected_response_obj = bson.json_util.loads('''{
+                "results": [
+                    {
+                        "friend_id_str": "461494372",
+                        "retrieved_by": "dummy_data",
+                        "record_retrieved_at": "Thu May 30 14:20:16 +0000 2013",
+                        "id_str": "461494360",
+                        "following": 1,
+                        "_id": {
+                            "$oid": "520d2f206ea62501d9c64ac9"
+                            }
+                        }
+                    ]
+                }''')
 
         print response.body
         obj = bson.json_util.loads(response.body)
 
         self.maxDiff = None
         self.assertEqual(obj, expected_response_obj)
-        # On successful, response is expected to redirect to /tutorial
-        # self.assertEqual(response.code, 302)
-        #self.assertTrue(response.headers['Location'].endswith('/tutorial'),
-        #                "response.headers['Location'] did not ends with /tutorial")
-
-# Apparently needed in this module for testing.main()
-#def all():
-#    return TestBucketHandler('test_deneme')
-#
-#if __name__ == '__main__':
-#    tornado.testing.main()
-
-#@pytest.fixture(scope="class")
-#def db_class(request):
-#    from .. import direnajinitdb
-#    class DummyDB:
-#        pass
-#    # set a class attribute on the invoking test context
-#    request.cls.db = DummyDB()
-#
-#@pytest.mark.usefixtures("db_class")
-#class FollowerHandlerTest(unittest.TestCase):
-#    from .. import FollowerHandler
-#    def test_method1(self):
-#        fh = FollowerHandler()
-#        fh.post
-#        assert hasattr(self, "db")
-#        assert 0, self.db   # fail for demo purposes
-#
-#    def test_method2(self):
-#        assert 0, self.db   # fail for demo purposes
 #
 ## http://localhost:9999/followers/list/view?user_id=461494372
