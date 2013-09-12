d69b23c35147f9ebe01accbed5f46fda21625e5d
Onur Gungor <onurgu@gmail.com>
2013-09-12 15:33:57 +0300

    Fix again!

--- fabfile.py
+++ fabfile.py
@@ -59,7 +59,7 @@ def setup_deployment_repo():
 ##`            run("git remote add deployment_repo_%s %s" % (env.direnaj['cank'], env.direnaj['cank_repo_uri']))
 
 def push():
-    local("git push %s %s_deployment" % (env.direnaj['deployment_repo_remote_name'], env.direnaj['environment']))
+    local("git push origin %s_deployment" % (env.direnaj['environment']))
 
 def prepare_deploy(environment=env.direnaj['environment'],
                    hostname=env.direnaj['hostname']):
