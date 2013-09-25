93a33782cd0afc394edd417ecbd1482dd65fd4aa
Onur Gungor <onurgu@gmail.com>
2013-09-25 07:13:36 +0300

    Bugfix in big client

--- www/app/js/controllers.js
+++ www/app/js/controllers.js
@@ -9,10 +9,12 @@ controller('MyCtrl1', [function() {
 .controller('StatusesFilterCtrl', ['$scope', '$http', function(scope, http) {
     /*scope.statuses = [{name:'a'}, {name: 'b'}];*/
     http.get('/statuses/filter', {
-        limit: 1,
-        campaign_id: 'default',
-        auth_user_id: 'direnaj',
-        auth_password: 'tamtam'
+        params: {
+            limit: 1,
+            campaign_id: 'syria',
+            auth_user_id: 'direnaj',
+            auth_password: 'tamtam'
+        }
     }).success(function(data, status, headers, config) {
         // this callback will be called asynchronously
         // when the response is available
