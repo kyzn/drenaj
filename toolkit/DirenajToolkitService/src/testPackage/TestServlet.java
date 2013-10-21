7e4672984cb295384d5d42a1be01d191e73de4f8
Onur Gungor <onurgu@gmail.com>
2013-10-22 01:41:36 +0300

    Some changes for nice deployment

--- toolkit/DirenajToolkitService/src/testPackage/TestServlet.java
+++ toolkit/DirenajToolkitService/src/testPackage/TestServlet.java
@@ -18,65 +18,65 @@ import javax.servlet.http.*;
 
 import direnajDriver.*;
 
-/** Simple servlet for testing. 
- * 
+/** Simple servlet for testing.
+ *
  * Uses DirenajDataHandler from direnajAdapter package
  */
 
-@WebServlet("/test")
+//@WebServlet("/test")
 public class TestServlet extends HttpServlet {
   /**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
 @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
 		  											throws ServletException, IOException {
-	
+
     response.setContentType("text/html; charset=UTF-8");
     PrintWriter out = response.getWriter();
-    
+
     // getting parameters from html form
     Map<String, String[]> params = request.getParameterMap();
-    
+
     String userId = params.get("userID")[0];
     String password = params.get("pass")[0];
     String campaignId = params.get("campaignID")[0];
     int skip = Integer.parseInt(params.get("skip")[0]);
     int limit = Integer.parseInt(params.get("limit")[0]);
-    
+
     String operation = params.get("operationType")[0];
-    
+
     System.out.println(operation);
-    
+
     // Start preparing objects
-    
-    // constructing the driver object that will handle our data retrieval 
+
+    // constructing the driver object that will handle our data retrieval
     // and processing requests, using DirenajDataHandler as a backbone
     DirenajDriver driver = new DirenajDriver(userId, password);
-    
-    
+
+
     ArrayList<String> tweetTexts = new ArrayList<String>();
     ArrayList<ArrayList<String>> allTags = new ArrayList<ArrayList<String>>();
     ArrayList<Map.Entry<String, Integer>> counts = new ArrayList<Map.Entry<String,Integer>>();
-    
+
     // building the result page
-    
+
     String retHtmlStr = "<!DOCTYPE html>\n" +
     					"<html>\n" +
     					"<head><title>Direnaj Test Center</title></head>\n" +
     					"<body bgcolor=\"#fdf5e6\">\n" +
     					"<h1>DirenajDriver Test</h1>\n" +
-    					"<p>CampaignID : <b>" + campaignId + "</b> | Operation : <b>" + operation + 
+    					"<p>CampaignID : <b>" + campaignId + "</b> | Operation : <b>" + operation +
     					"</b> | Limit : <b>" + limit + "</b></p><hr>\n";
-    
-    
+
+
     try {
     	if(operation.equals("getTags")) {
-    		
+
     		allTags = driver.collectHashtags(campaignId, skip, limit);
-    		
+
     		retHtmlStr += "<ul>";
     		for (ArrayList<String> singleTweetTags: allTags) {
     			//if(singleTweetTags.size() != 0) {
@@ -88,21 +88,21 @@ public class TestServlet extends HttpServlet {
     			//}
     	    }
     		retHtmlStr += "</ul>";
-    		
+
     	} else if(operation.equals("getTagCounts")) {
-    		
-    		
+
+
     		counts = driver.countHastags(campaignId, skip, limit);
-    		
+
     		PrintWriter pw = new PrintWriter(new File("/home/caner/IBMWordCloud/tags.txt"));
-    		
-    		
+
+
     		boolean skipFirst = true;
-    		
+
     		retHtmlStr += "<table width=100% border=0><tr valign=top><td><ul>";
     		for(Map.Entry<String, Integer> entry : counts) {
     	    	retHtmlStr += "<li><font size=\"2\">(" + entry.getKey() + " : " + entry.getValue() + ")</font></li>";
-    	    	
+
     	    	if(skipFirst) {
     	    		skipFirst = false;
     	    		continue;
@@ -111,55 +111,55 @@ public class TestServlet extends HttpServlet {
     	    	}
     	    }
     		retHtmlStr += "</ul></td><td align=right>";
-    		
+
     		pw.close();
 
-    	    
+
     	    String cmd = "java -jar /home/caner/IBMWordCloud/ibm-word-cloud.jar "
     	    		+ "-c /home/caner/IBMWordCloud/caner.conf -w 800 -h 600 "
     	    		+ "-i /home/caner/IBMWordCloud/tags.txt "
     	    		+ "-o /home/caner/IBMWordCloud/images/caner.png";
-    	    
+
     	    Process proc = Runtime.getRuntime().exec(new String[] {"/bin/sh", "-c", cmd});
-    	    
+
     	    BufferedReader br = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-    	    
+
     	    String line = null;
     	    while((line = br.readLine()) != null) {
     	    	System.out.println(line);
     	    }
-    	    
+
             proc.waitFor();
-            
+
             retHtmlStr += "<img src=\"/images/caner.png\"/>   </td></tr></table>";
-            
-    		
+
+
     	} else if(operation.equals("getTweetTexts")) {
-    		
+
     		tweetTexts = driver.collectTweetTexts(campaignId, skip, limit);
-    		
+
     		retHtmlStr += "<ul>";
     		for(String tweetText : tweetTexts) {
     			retHtmlStr += "<li><font size=\"2\">" + tweetText + "</font></li>";
     		}
     		retHtmlStr += "</ul>";
-    		
+
     	} else if(operation.equals("getSingleTweet")) {
-    		
+
     		retHtmlStr += driver.getSingleTweetInfo(campaignId, skip, limit);
-    		
+
     	} else {
     		retHtmlStr += "OPERATION NOT SUPPORTED!";
     	}
-    	
+
     } catch(Exception e) {
     	out.println(e.getMessage());
     	out.close();
     	System.out.println(e);
     }
-    
-    
-    
+
+
+
     out.println(retHtmlStr + "</body></html>");
     out.close();
   }
