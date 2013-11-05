c7c14e1bfd72f0d0952a510984b25cdc5ea045c4
Onur Gungor <onurgu@gmail.com>
2013-11-05 16:48:08 +0200

    Some cleanup at drnj_time.py

--- direnaj/drnj_time.py
+++ direnaj/drnj_time.py
@@ -34,3 +34,12 @@ def py_utc_time2drnj_time(time_str):
     import calendar
     t = calendar.timegm(t)
     return py_time2drnj_time(t)
+
+
+def drnj_time2py_time(dt):
+    """
+    dt : days from 1-Jan-0000
+    returns seconds from 1 Jan  1970
+    """
+    t = (dt-number_of_days_from_1_Jan_0000_to_1_Jan_1970)*24.0*60*60
+    return t
