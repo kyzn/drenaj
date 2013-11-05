183975217e6a3798df8ed253fab227ffa57fb8e3
Onur Gungor <onurgu@gmail.com>
2013-11-05 16:48:50 +0200

    Some code cleanup.

--- direnaj/drnj_time.py
+++ direnaj/drnj_time.py
@@ -3,27 +3,22 @@
 
 import time
 
-number_of_days_from_1_Jan_0000_to_1_Jan_1970 = 719529.0;
+number_of_days_from_1_Jan_0000_to_1_Jan_1970 = 719529.0
+
 
 def now_in_drnj_time():
     t = time.time()
     return py_time2drnj_time(t)
 
-def drnj_time2py_time(dt):
-	"""
-	 dt : days from 1-Jan-0000
-	 returns seconds from 1 Jan  1970
-	"""
-	t = (dt-number_of_days_from_1_Jan_0000_to_1_Jan_1970)*24.0*60*60;
-	return t
 
 def py_time2drnj_time(t):
-	"""
-	 t : seconds from 1-Jan-1970
-	 returns days from 1 Jan 0000
-	"""
-	dt = number_of_days_from_1_Jan_0000_to_1_Jan_1970 + t/(24.0*60*60);
-	return dt
+    """
+    t : seconds from 1-Jan-1970
+    returns days from 1 Jan 0000
+    """
+    dt = number_of_days_from_1_Jan_0000_to_1_Jan_1970 + t/(24.0*60*60)
+    return dt
+
 
 def py_utc_time2drnj_time(time_str):
     """
