28af57572288a5c4935023d853d5c93c42b671a1
Onur Gungor <onurgu@gmail.com>
2013-11-07 01:43:39 +0200

    Small quick fix (3)

--- direnaj/direnajmongomanager.py
+++ direnaj/direnajmongomanager.py
@@ -31,64 +31,66 @@ tweets_coll.create_index([('campaign_id', pymongo.ASCENDING), ('tweet.created_at
 for key in colls.keys():
     colls[key].create_index([('campaign_id', pymongo.ASCENDING), ('date', pymongo.ASCENDING), ('key', pymongo.ASCENDING)])
 
-def insert_tweet(tweet_obj):
+def insert_tweet(tweet_obj_array):
 
     # actual tweet insertion
-    tweets_coll.insert(tweet_obj)
+    tweets_coll.insert(tweet_obj_array)
 
-    # build the analytics
-    freq = {}
+    for tweet_obj in tweet_obj_array:
 
-    campaign_id = tweet_obj['campaign_id']
-    freq['campaigns'] = {campaign_id: 1}
+        # build the analytics
+        freq = {}
 
-   # freq['tokens'] = {'ali': 1, 'veli': 1}
+        campaign_id = tweet_obj['campaign_id']
+        freq['campaigns'] = {campaign_id: 1}
 
-    freq['hashtags'] = {}
-    if 'entities' in tweet_obj and 'hashtags' in tweet_obj['entities']:
-        for hashtag in tweet_obj['entities']['hashtags']:
-            if 'text' in hashtag:
-                if hashtag['text'] in freq['hashtags']:
-                    freq['hashtags'][hashtag] += 1
+    # freq['tokens'] = {'ali': 1, 'veli': 1}
+
+        freq['hashtags'] = {}
+        if 'entities' in tweet_obj and 'hashtags' in tweet_obj['entities']:
+            for hashtag in tweet_obj['entities']['hashtags']:
+                if 'text' in hashtag:
+                    if hashtag['text'] in freq['hashtags']:
+                        freq['hashtags'][hashtag] += 1
+                    else:
+                        freq['hashtags'][hashtag] = 1
                 else:
-                    freq['hashtags'][hashtag] = 1
-            else:
-                # log this missing attribute.
-                pass
+                    # log this missing attribute.
+                    pass
 
-    freq['mentions'] = {}
-    if 'entities' in tweet_obj and 'user_mentions' in tweet_obj['entities']:
-        for mention in tweet_obj['entities']['user_mentions']:
-            if 'id_str' in mention:
-                item_key = "|".join([mention['id_str'], mention['screen_name']])
-                if item_key in freq['mentions']:
-                    freq['mentions'][item_key] += 1
+        freq['mentions'] = {}
+        if 'entities' in tweet_obj and 'user_mentions' in tweet_obj['entities']:
+            for mention in tweet_obj['entities']['user_mentions']:
+                if 'id_str' in mention:
+                    item_key = "|".join([mention['id_str'], mention['screen_name']])
+                    if item_key in freq['mentions']:
+                        freq['mentions'][item_key] += 1
+                    else:
+                        freq['mentions'][item_key] = 1
                 else:
-                    freq['mentions'][item_key] = 1
-            else:
-                # log this missing attribute.
-                pass
+                    # log this missing attribute.
+                    pass
 
-    freq['urls'] = {'http://url1': 1, 'http://url2': 1}
-    if 'entities' in tweet_obj and 'urls' in tweet_obj['entities']:
-        for url in tweet_obj['entities']['urls']:
-            if 'expanded_url' in url:
-                item_key = url['expanded_url']
-                if item_key in freq['urls']:
-                    freq['urls'][item_key] += 1
+        freq['urls'] = {}
+        if 'entities' in tweet_obj and 'urls' in tweet_obj['entities']:
+            for url in tweet_obj['entities']['urls']:
+                if 'expanded_url' in url:
+                    item_key = url['expanded_url']
+                    if item_key in freq['urls']:
+                        freq['urls'][item_key] += 1
+                    else:
+                        freq['urls'][item_key] = 1
                 else:
-                    freq['urls'][item_key] = 1
-            else:
-                # log this missing attribute.
-                pass
+                    # log this missing attribute.
+                    pass
 
-    today_str = time.strftime('%Y-%m-%d')
+        today_str = time.strftime('%Y-%m-%d')
 
-    hour = time.strftime('%H')
-    minute = "%04d" % (int(hour)*60 + int(time.strftime('%M')))
+        hour = time.strftime('%H')
+        minute = "%04d" % (int(hour)*60 + int(time.strftime('%M')))
 
-    for key in freq.keys():
-        for item in freq[key].keys():
-            count = freq[key][item]
-            colls[key].update({'campaign_id': campaign_id, 'date': today_str, 'key': item}, {'$inc': {('hour.%s' % hour): count, ('minute.%s' % minute): count}}, upsert=True)
+        for key in freq.keys():
+            for item in freq[key].keys():
+                count = freq[key][item]
+                colls[key].update({'campaign_id': campaign_id, 'date': today_str, 'key': item}, {'$inc': {('hour.%s' % hour): count, ('minute.%s' % minute): count}}, upsert=True)
 
