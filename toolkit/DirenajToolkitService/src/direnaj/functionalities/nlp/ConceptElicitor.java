0e1a6deaf2d7c3ee7afac8ea3f890531c1a87551
Caner Derici <canerderici@gmail.com>
2014-01-05 16:31:54 +0200

    minor fix + improved tweet normalization

--- toolkit/DirenajToolkitService/src/direnaj/functionalities/nlp/ConceptElicitor.java
+++ toolkit/DirenajToolkitService/src/direnaj/functionalities/nlp/ConceptElicitor.java
@@ -2,14 +2,17 @@ package direnaj.functionalities.nlp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
+import direnaj.domain.User;
 import direnaj.functionalities.nlp.WordStringUtils;
 import direnaj.functionalities.nlp.TweetParseUtil;
 
 public class ConceptElicitor {
 	
 	static ArrayList<String> getCleanWords(String rawTweet) {
-		String cleanTweet = WordStringUtils.removeStopwords(rawTweet);
+		String cleanTweet = WordStringUtils.normalizeTweetWords(rawTweet);
+		cleanTweet = WordStringUtils.removeStopwords(cleanTweet);
 		
 		cleanTweet = TweetParseUtil.onlyText(cleanTweet);
 		
@@ -25,27 +28,27 @@ public class ConceptElicitor {
 		return words;
 	}
 	
-	
 	static ArrayList<String> tweetRelatedConcepts(String tweet) {
 		
 		List<String> tags = TweetParseUtil.getHashtagsInText(tweet);
 		
 		ArrayList<String> words = getCleanWords(tweet);
 		
-		/*
+		
 		for (String word : words) {
 			System.out.println(word);
 		}
-		*/
-		
-		Synset s = new Synset
-		
 		
 		
 		return new ArrayList();
 	}
 	
+	static ArrayList<String> communityConcept(Vector<User> users) {
+		return null;
+		
+	}
+	
 	public static void main(String[] args) {
-		ConceptElicitor.tweetRelatedConcepts("Who was Albert Wohlstetter? What was \"Team B\"? Who wants to lead the U.S. into Syria? You should know: 'In the... http://t.co/QkF6Ojozjx");
+		ConceptElicitor.tweetRelatedConcepts("Who was Albert 2day Wohlstetter? What was \"Team B\"? Who wants to lead the U.S. into Syria? You should know: 'In the... http://t.co/QkF6Ojozjx");
 	}
 }
