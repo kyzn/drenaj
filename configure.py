f9bc10767d433db3197c1b4d4f4f9e3bea9137a5
Onur Gungor <onurgu@gmail.com>
2014-07-11 21:34:42 +0300

    Config files are modified to be in line with the new refactorings. (3)

--- configure.py
+++ configure.py
@@ -3,6 +3,8 @@ from string import Template
 
 import argparse
 
+import os
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='configuration generator')
     parser.add_argument('config_yaml_file', help='config file name in yaml format')
@@ -10,6 +12,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # read config.yaml and generate config.py
     conf = yaml.load(file(args.config_yaml_file, "r"))
+
+    # set the project root dir as the current dir.
+    conf['project_root_dir'] = os.getcwd()
+
+    # TODO: completely inportabe :) for now.
+    # It is not a problem because it is only used by direnajinitdb.py
+    # set the directory which mongo binary resides
+    conf['mongo_bin_dir'] = '/usr/bin'
+
     default_conf = yaml.load(file("host-configs/default-configs.yaml", "r"))
     config_document_template = file("direnaj/direnaj_api/config/config.py.tmpl", 'r').read()
     config_py_document = Template(config_document_template).substitute(default_conf, **conf)
