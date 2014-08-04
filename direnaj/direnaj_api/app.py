f6f1f42467262198307baf2947763e2853543c16
Onur Gungor <onurgu@gmail.com>
2014-08-04 15:22:09 +0300

    Let's use direnaj_routes_config.py as the definitive place where we keep the routes.

--- direnaj/direnaj_api/app.py
+++ direnaj/direnaj_api/app.py
@@ -37,16 +37,6 @@ import logging
 import logging.handlers
 from tornado.log import LogFormatter
 
-from direnaj_api.handlers.followerhandler import *
-from direnaj_api.handlers.statuseshandler import *
-from direnaj_api.handlers.userprofileshandler import *
-from direnaj_api.handlers.campaignshandler import *
-from direnaj_api.handlers.taskshandler import *
-
-from direnaj_api.handlers.schedulerMainHandler import SchedulerMainHandler
-from direnaj_api.handlers.schedulerMainHandler import SchedulerReportHandler
-from direnaj_api.handlers.schedulerMainHandler import SchedulerProfilesHandler
-
 from direnaj_api.utils.direnajmongomanager_new import DirenajMongoManager
 
 logger = logging.getLogger()
@@ -70,24 +60,8 @@ class Application(tornado.web.Application):
         if not overrides:
             overrides = {}
 
-        handlers = [
-            (r"/scheduler/suggestUseridToGet_profiles", SchedulerProfilesHandler),
-            (r"/scheduler/suggestUseridToGet_(friends|followers)", SchedulerMainHandler),
-            (r"/scheduler/reportProtectedUserid", SchedulerReportHandler),
-
-            (r"/(friends|followers)/(ids|list)/?(store|view)?", FollowerHandler),
-
-            (r"/statuses/(store|view|filter|retweets)", StatusesHandler),
-            # (r"/user/(store|view)", UserSingleProfileHandler),
-
-            (r"/profiles/(store)", UserProfilesHandler),
-            (r"/profiles/(view)/(campaign|user)", UserProfilesHandler),
-
-            (r"/campaigns/(new|list|filter|histograms)", CampaignsHandler),
-            (r"/campaigns/(view)/?(freqs|histograms|watched_users)?", CampaignsHandler),
-
-            (r"/tasks/(harvest|crawl)", TasksHandler),
-        ]
+        # Creating a `handlers' variable using direnaj_routes_config.py
+        from direnaj_api.config.direnaj_routes_config import routes_config as handlers
 
         # default settings for the application
         settings = {
