a095c227034454c7e6ae2a4c021710e65589ea1a
Eren Türkay <turkay.eren@gmail.com>
2014-08-22 19:26:32 +0300

    api/tests: add test runner and Makefile entries for test generation
    Tests can now be run with "make test". To get a report of line-by-line
    execution and coverage, use "make report" to generate it.
    
    Delete old test files which are not used anymore.

---
+++ direnaj/direnaj_api/tests/run.py
@@ -0,0 +1,18 @@
+__author__ = 'Eren Turkay <turkay.eren@gmail.com>'
+
+import unittest
+
+TEST_DIR = 'tests/'
+TEST_PATTERN = 'test_*.py'
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    for all_test_suite in unittest.defaultTestLoader.discover(TEST_DIR, pattern=TEST_PATTERN):
+        for test_suite in all_test_suite:
+            suite.addTests(test_suite)
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main()
