9cc5664316f142d35fa3c7f5c41087eb75f7c6b1
Onur Gungor <onurgu@gmail.com>
2015-02-23 20:21:58 +0200

    Bugfix. UNIQUEPATHNOTUNIQUE

--- direnaj/direnaj_api/utils/direnajneo4jmanager.py
+++ direnaj/direnaj_api/utils/direnajneo4jmanager.py
@@ -77,9 +77,14 @@ def init_user_to_graph_aux(campaign_node, user):
 
     print 'init user to graph aux Giris'
 
+    from py2neo.core import GraphError, ClientError
+
     user_node = upsert_user(user)
     campaign_rel = Relationship(campaign_node, "OBSERVES", user_node)
-    graph.create_unique(campaign_rel)
+    try:
+        graph.create_unique(campaign_rel)
+    except (GraphError, ClientError), e:
+        print("PROBABLY A UNIQUEPATHNOTUNIQUE error")
 
     timeline_task_state_rel = \
         graph.cypher.execute(
